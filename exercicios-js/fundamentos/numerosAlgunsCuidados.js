9b04c0fb20c01fe752857b46a7e8c1f8e3be3fe9
André Quintiliano Filho <drequintiliano@gmail.com>
2020-01-02 21:16:30 -0300

    primeiras aulas js

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // infinity
+console.log("10" / 2)
+console.log("Show!" * 2); //NaN --not a number
+console.log(0.1 + 0.7); // ANSI/IEEE Standard for Binary Floating-Point Arithimetic
+//console.log(10.toString());
+console.log((10.345).toFixed(2));
---
+++ exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+   let qualquer = 'legal'
+   console.log(qualquer);
+   console.log(typeof qualquer);
+
+   qualquer = 3.1516
+   console.log(qualquer);
+   console.log(typeof qualquer);
+
+   //evitar nome generico
+   let valor = ''
+   let numero = 1
+   let pqp = false //produto quimico perigoso
