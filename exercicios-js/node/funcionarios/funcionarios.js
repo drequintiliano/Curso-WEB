d199a4a211c87cd7908594dbb78c80ebbb369819
André Quintiliano Filho <drequintiliano@gmail.com>
2020-01-27 20:11:58 -0300

    node continuacao

---
+++ exercicios-js/node/funcionarios/funcionarios.js
@@ -0,0 +1,22 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios') //axios - client http - faz requisição
+
+
+const findChina = a => a.pais = 'China'
+const findMulher = a => a.genero = 'F'
+
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+
+    const resultado = funcionarios
+        .filter(findChina)
+        .filter(findMulher)
+        .reduce(menorSalario)
+
+    console.log(resultado);
+
+})
