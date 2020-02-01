3352b378d6e43cd2c66db047efb9094fd9edefc7
André Quintiliano Filho <drequintiliano@gmail.com>
2020-02-01 19:53:22 -0300

    finalizando ESnext

---
+++ exercicios-js/esnext/promise.js
@@ -0,0 +1,12 @@
+function falarDepoisDe(segundos, frase) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(frase)
+        }, segundos * 1000);
+    })
+}
+
+falarDepoisDe(3, 'que legal!')
+    .then(frase => frase.concat('?!?'))
+    .then(outraFrase => console.log(outraFrase))
+    .catch(e => console.log(e)) //sera chamada sempre que chamar o erro "reject"
